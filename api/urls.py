c740f60574f2802ac23913ef5d7b7314723fa8ad
gautamsinh1234 <gautamsinhmakwana143@gmail.com>
2022-03-10 15:21:09 +0530

    home page add

--- api/urls.py
+++ api/urls.py
@@ -22,21 +22,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-   path('apioverview/',views.Apioverview.as_view(),name='api-overview'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-   path('res_data_id/<int:id>',views.ResDataWithID.as_view(),name='data_with_id'),
-   path('res_data/',views.RestData.as_view(),name='all-data'),
-   path('cat_data_id/<int:id>',views.CatDataWithId.as_view(),name='cat_data_with_id'),
-   path('cat_data/',views.CatData.as_view(),name='cat-all-data'),
-   path('iteam_data_id/<int:id>',views.IteamWithID.as_view(),name='iteam_data_with_id'),
-   path('iteam_data/',views.IteamData.as_view(),name='iteam-all-data'),
-   path('All_Items_with_res_cat/',views.All_Items_with_res_cat.as_view(),name='All_Items_with_res_cat'),
-   path('restaurants_list_contain_iteam/', views.RestaurantList.as_view(), name='restaurants'),
-   path('Restaurant_details_with_count/<int:id>', views.Restaurant_details_with_count.as_view(), name='Restaurant_details_with_count'),
-   path('Iteam_order_by_price/', views.Iteam_order_by_price.as_view(), name='Iteam_order_by_price'),
-   path('Res_Greater_500/', views.Res_Greater_500.as_view(), name='Res_Greater_500'),
-   path('all_iteam_wich_cat_using_res/<int:id>', views.all_iteam_wich_cat_using_res.as_view(), name='all_iteam_wich_cat_using_res'),
+   path('',views.home,name='home'),
+  
 ]        
 
 # router = DefaultRouter()
--- api/views.py
+++ api/views.py
@@ -1,10 +1,16 @@
 from urllib import response
+from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from .models import Restaurant,Cat_Res,Iteams
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
-from .serializers import all_iteam_with_cat_using_res_Serializer,Res_Greater_500_Serializer,Restaurant_details_with_count_Serializer,IteamListSerializer,UserSerializer,Cat_resSerializer, RestaurantSerializer,IteamSSerializer,CustomeIteamSSerializer
-from rest_framework import viewsets, generics
+
 from django.db.models import Sum
+
+
+
+
+def home(request):
+    return render(request,'api/home.html')
